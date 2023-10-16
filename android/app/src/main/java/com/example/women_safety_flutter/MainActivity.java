163de4c15164a01ed56dc90e3ea58a825db325d1
MirInjamamul <injamul120221@gmail.com>
2023-10-17 00:36:57 +0600

    permission added

--- android/app/src/main/java/com/example/women_safety_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/women_safety_flutter/MainActivity.java
@@ -7,6 +7,7 @@ package com.example.women_safety_flutter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import io.flutter.embedding.android.FlutterActivity;
@@ -14,6 +15,7 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import android.Manifest;
 import android.app.KeyguardManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.AdvertiseCallback;
@@ -21,6 +23,7 @@ import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.ParcelUuid;
 import android.util.Log;
@@ -28,8 +31,8 @@ import android.widget.Toast;
 
 import com.example.women_safety_flutter.service.PanicService;
 
-@android.support.annotation.RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-public class MainActivity extends FlutterActivity{
+
+public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "safety/panic";
 
     // BLE Advertising
@@ -51,12 +54,12 @@ public class MainActivity extends FlutterActivity{
 
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
                 .setMethodCallHandler((call, result) -> {
-                    if (call.method.equals("startPanicService")){
+                    if (call.method.equals("startPanicService")) {
                         Log.d("MAIN_ACTIVITY", "Start Service");
                         startService();
                         // Start BLE Advertising
                         startAdvertising();
-                    }else if (call.method.equals("stopPanicService")){
+                    } else if (call.method.equals("stopPanicService")) {
                         Log.d("MAIN_ACTIVITY", "Stop Service");
                         stopService();
 
@@ -71,24 +74,45 @@ public class MainActivity extends FlutterActivity{
         Intent serviceIntent = new Intent(getActivity(), PanicService.class);
         serviceIntent.putExtra("inputExtra", input);
 
-        if(Build.VERSION.SDK_INT >= 26)
+        if (Build.VERSION.SDK_INT >= 26)
             ContextCompat.startForegroundService(this, serviceIntent);
         else
             getContext().startService(serviceIntent);
     }
 
-    private void startAdvertising(){
-        if(bluetoothAdapter != null){
-            if(bluetoothAdapter.isMultipleAdvertisementSupported()){
+    private void startAdvertising() {
+        if (bluetoothAdapter != null) {
+            if (bluetoothAdapter.isMultipleAdvertisementSupported()) {
                 BluetoothLeAdvertiser advertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
                 advertiser.startAdvertising(settings, advertiseData, advertiseCallback);
             }
-        }else{}
+        } else {
+        }
     }
 
-    private void stopAdvertising(){
-        if(bluetoothAdapter != null){
+    private void stopAdvertising() {
+        if (bluetoothAdapter != null) {
             BluetoothLeAdvertiser advertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             advertiser.stopAdvertising(advertiseCallback);
         }
     }
